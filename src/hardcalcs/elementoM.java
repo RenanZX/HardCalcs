fd3690c5ee853a99beb76d424e4cf6f12e60d7e0
RenanZX <renangodoimedeiros@gmail.com>
2017-01-14 19:38:45 -0200

    Add files via upload
    adicionado a opçao de calcular matrizes

---
+++ src/hardcalcs/elementoM.java
@@ -0,0 +1,8 @@
+
+package hardcalcs;
+/*classe elemento*/
+public class elementoM {
+    int dado = 0;   /*dado que sera guardado*/
+    elementoM proxC = null; /*elemento que representa o ponteiro para proxima coluna*/
+    elementoM proxL = null; /*elemento que representa o ponteiro para proxima linha*/
+}
